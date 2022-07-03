9a418823c1bd6fa3a3df79c29a84372e83080729
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-07-04 00:24:33 +0900

    change color of username and hostname

--- functions/__bobthefish_colors.fish
+++ functions/__bobthefish_colors.fish
@@ -545,7 +545,8 @@ function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthe
       set -l base0D  EBCB8B
       set -l base0E  A3BE8C
       set -l base0F  B48EAD
-      set -l base10  c0c0c0
+      set -l base10  909090
+      set -l base11  600000
 
       set -l colorfg $base00
 
@@ -572,8 +573,8 @@ function __bobthefish_colors -S -a color_scheme -d 'Define colors used by bobthe
       set -x color_k8s                      $base02 $colorfg --bold
       set -x color_aws_vault                $base0A $base0D --bold
       set -x color_aws_vault_expired        $base0A $base0B --bold
-      set -x color_username                 $base10 $base0B --bold
-      set -x color_hostname                 $base10 $base0B
+      set -x color_username                 $base10 $base11 --bold
+      set -x color_hostname                 $base10 $base11
       set -x color_rvm                      $base09 $colorfg --bold
       set -x color_node                     $base09 $colorfg --bold
       set -x color_virtualfish              $base09 $colorfg --bold
